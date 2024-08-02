077d15199e95f054bc96a51e68b80e9d2bba8df4
wpjml <yuuki9s030927@gmail.com>
2024-08-02 23:22:53 +0900

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:ffi';
+import 'question.dart';
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userpickedanswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if (quizBrain.inFinished() == true) {
+        Alert(
+                context: context,
+                title: "RFLUTTER",
+                desc: "Quizzler is finished.")
+            .show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      } else {
+        if (userpickedanswer == correctAnswer) {
+          print('正解');
+          setState(
+            () {
+              quizBrain.NextQuestion();
+              scoreKeeper.add(
+                Icon(
+                  Icons.check,
+                  color: Colors.green,
+                ),
+              ); //The user picked true.
+            },
+          );
+        } else {
+          print('不正解');
+          setState(
+            () {
+              quizBrain.NextQuestion();
+              scoreKeeper.add(
+                Icon(
+                  Icons.close,
+                  color: Colors.red,
+                ),
+              ); //The user picked true.
+            },
+          );
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext constext) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Container(
+              color: Colors.green,
+              child: TextButton(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(true);
+                },
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Container(
+              color: Colors.red,
+              child: TextButton(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(false);
+                  //The user picked false.
+                },
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: scoreKeeper),
+        )
+      ],
+    );
+  }
+}
